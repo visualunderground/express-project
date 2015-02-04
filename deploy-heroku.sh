5e2151ff07f83e40c1756bafbeff9b0e98539f61
visualunderground <tom@visualunderground.co.uk>
2015-02-04 12:43:39 +0000

    changed rebase to merge

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -3,10 +3,11 @@
 # "Automatic deploys from GitHub" are enabled in Heroku monitoring the 'deploy' branch.
 
 git checkout deploy
-git rebase master
+# git rebase master
+git merge master
 grunt build
 git add .
 git add -f ./app/public
-git commit -m 'Syncing with master and adding public files'
+git commit -m 'Merging master into deploy and adding public files'
 git push
 git checkout master
