0fc995a4c36df4ca648571e82fb6f7c9926ba267
visualunderground <tom@visualunderground.co.uk>
2015-02-04 12:50:34 +0000

    removed git add . as is now covered by merge master

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -6,8 +6,8 @@ git checkout deploy
 # git rebase master
 git merge master -m 'Merging master into deploy'
 grunt build
-git add .
+# git add .
 git add -f ./app/public
-git commit -m 'Adding public files'
+git commit -m 'Adding app/public files'
 git push
 git checkout master
